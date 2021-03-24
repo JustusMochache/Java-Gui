ade0d7eb44dd3bf6e8f37d213a81b8441d79c2a3
moringaschool <justusm10@gmail.com>
2021-03-24 15:25:22 +0300

    Create anew class and create an instance for Jframe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,18 @@ public class Main {
         // Jframe a gui window to add components to
         
         
-        JFrame frame = new JFrame(); //creates a frame
-        frame.setTitle("Jframe first GUI ");
-        
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);// set x-dimension and y-dimension
-        frame.setVisible(true); //make frame visible
+//        JFrame frame = new JFrame(); //creates a frame
+//        frame.setTitle("Jframe first GUI ");
+//
+//
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500,500);// set x-dimension and y-dimension
+//        frame.setVisible(true); //make frame visible
+//
+//        ImageIcon image = new ImageIcon("tower.jpeg"); //create an image icon
+//        frame.setIconImage(image.getImage()); //change icon fram
+//    frame.getContentPane().setBackground(new Color(230, 227, 255)); //change background color
 
-        ImageIcon image = new ImageIcon("tower.jpeg");
-        frame.setIconImage(image.getImage());
-
-        frame.getContentPane().setBackground(new Color(230, 227, 255));
+        MyFrame myFrame = new MyFrame();
     }
 }
---
+++ src/main/java/MyFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MyFrame extends JFrame {
+
+    MyFrame(){
+//        Jthis this = new Jthis(); //creates a this
+        this.setTitle("Jthis first GUI ");
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);// set x-dimension and y-dimension
+        this.setVisible(true); //make this visible
+
+        ImageIcon image = new ImageIcon("tower.jpeg"); //create an image icon
+        this.setIconImage(image.getImage()); //change icon this
+
+        this.getContentPane().setBackground(new Color(230, 227, 255)); //change background color
+    }
+
+}
